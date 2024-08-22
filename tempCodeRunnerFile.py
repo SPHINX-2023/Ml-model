c03d8f39582f5dcf70702322cc900a4b3bac136e
Ketan-singh-2110156 <ketan2110156@akgec.ac.in>
2024-08-22 22:28:32 +0530

    all files added

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from keras.models import load_model
+import json
+import random
+
+app = Flask(__name__)
+
+def lo_model(model_add):
+    try:
+        model = load_model(model_add)
+        return model
+    except Exception as e:
+        raise Exception(f"Failed to load model: {str(e)}")
+
+pre_model = lo_model('exp_02.h5')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.json
+        if 'input_data' not in data:
+            return jsonify({'error': 'Input data is missing'}), 400
+        input_data = np.array(data['input_data'][0])
+        prediction = pre_model.predict(input_data.reshape(1,14,1)) 
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
